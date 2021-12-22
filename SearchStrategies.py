8925d4d821189575d6a07619c2a01ee053fae46a
Satwik Srivastava <satwiksri27@gmail.com>
2021-12-22 23:16:41 +0530

    Alpha 0.38: Completed BFS for Tree and Graph

--- SearchStrategies.py
+++ SearchStrategies.py
@@ -1,23 +1,29 @@
 """
 Author: Satwik Srivastava (^__^)
-Profiles >>==>
+
+Profiles -> 
     LinkedIn : https://www.linkedin.com/in/satwiksrivastava/
     twitter  : @Satwik_9
     github   : https://github.com/Satwik-S9
 
 Description: In this script I write down all the basic search strategies employed in Artificial Intelligence 
-! private: git remote search 
 """
+# todo: Complete the class, add 'BST' and 'nonBST' options
 
- 
-class Node:
+
+#* Global Version container
+__version__ = "0.38"
+
+
+#* === TREE DATA-STRUCTURE === *#
+class TreeNode:
     """[TreeNode]
-    - Created to create Trees for traversal problems.
-    - Has parent and weight containers
-    - Inherited by the Tree class
+        - Created to create Trees for traversal problems.
+        - Has parent and weight containers
+        - Inherited by the Tree class
     """
 
-    def __init__(self, value, weight=None):
+    def __init__(self, value=None, weight=None):
         self.value = value
         self.weight = weight
         self.left = None
@@ -25,32 +31,80 @@ class Node:
         self.parent = None
 
     def __repr__(self):
-        return f"\x1B[3m<Node({self.value}, {self.weight}), Parent: {self.parent}>\x1B[0m"
+        return f"\x1B[3m<TreeNode({self.value}, {self.weight}), Parent: {self.parent}>\x1B[0m"
 
     def __str__(self):
         return repr(self)
-    
 
-# todo: Complete the class, add 'BST' and 'nonBST' options
-class Tree(Node):
+#* ==== HELPER FUNCTIONS FOR THE CLASS ==== *#
+#  ----------------------------------------  #
+#! Needs to be checked
+def display_keys(node, space='\t', level=0):
+    """Display the Binary Tree"""
+    # if the node is empty
+    if node is None:
+        print(space*level + '∅')
+        return
 
-    def initialize(self, tree_tuple: tuple, weight_tuple: tuple = None):
-        root = Node('None')
-        if weight_tuple is None:
-            if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
-                root = Node(tree_tuple[1], 1)
-                root.left = self.initialize(tree_tuple[0])
-                root.right = self.initialize(tree_tuple[2])
+    #if the node is a leaf
+    if node.left is None and node.right is None:
+        print(space*level + str(node.value))
 
-        elif weight_tuple is not None:
-            if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
-                root = Node(tree_tuple[1], weight_tuple[1])
-                root.left = self.initialize(tree_tuple[0], weight_tuple[0])
-                root.right = self.initialize(tree_tuple[2], weight_tuple[2])
-
-        return root
+    # if the node has children
+    display_keys(node.right, space, level+1)
+    print(space*level + str(node.value))
+    display_keys(node.left, space, level+1)
 
 
+def height(node):
+    if node is None:
+        return 0
+
+    return 1 + max(height(node.left), height(node.right))
+
+
+def size(node):
+    if node is None:
+        return 0
+
+    return 1 + size(node.left) + size(node.right)
+
+def create_tree(tree_tuple: tuple, weight_tuple: tuple = None):
+    # self.root = TreeNode()
+    if weight_tuple is None:
+        if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
+            root = TreeNode(tree_tuple[1], 1)
+            root.left = create_tree(tree_tuple[0])
+            root.right = create_tree(tree_tuple[2])
+            
+        else:
+            root = TreeNode(tree_tuple)
+
+    elif weight_tuple is not None:
+        if isinstance(tree_tuple, tuple) and len(tree_tuple) == 3:
+            root = TreeNode(tree_tuple[1], weight_tuple[1])
+            root.left = create_tree(tree_tuple[0], weight_tuple[0])
+            root.right = create_tree(tree_tuple[2], weight_tuple[2])
+        
+        else:
+            root = TreeNode(tree_tuple)
+            
+    else:
+        root = TreeNode(tree_tuple)
+
+    return root
+
+def to_tuple(root):
+    if root is None:
+        return None
+    if root.left is None and root.right is None:
+        return root.value
+    return TreeNode.to_tuple(root.left), root.value, TreeNode.to_tuple(root.right)
+
+
+
+#* === GRAPH DATA-STRUCTURE === *#
+#  ----------------------------  #
 class Graph:
     """[Graph]
     - Create a graph given number of nodes and edges:(list of tuples which may include weights aside 
@@ -99,46 +153,123 @@ class Graph:
         return repr(self)
 
 
-
-#* UNINFORMED SEARCH ALGORITHM
+#* === UNINFORMED SEARCH ALGORITHM === *#
+#  -----------------------------------  #
 class BFS:
-    def __init__(self, root, struct, type='graph'):
+    def __init__(self, struct, root=None, type='graph'):
         self.root = root
         self.struct = struct
         self.type = type
+        if self.type == 'graph' and self.root is None:
+            raise ValueError('root argument requiured')
 
     def traverse(self, verbose=False):
-        assert type(self.struct) == Graph
-        queue = []  # maintain a queue of visited nodes
-        discovered = [False] * len(self.struct.data)  # Array to keep track whether node is discovered
-        distance = [None] * len(self.struct.data)
-        parent = [None] * len(self.struct.data)
-        
-        discovered[self.root] = True
-        queue.append(self.root)
-        distance[self.root] = 0
-        idx = 0
-        while idx < len(queue):
-            # dequeue
-            value = queue[idx]
-            idx += 1
-            
-            # traversal
-            for node in self.struct.data[value]:
-                if not discovered[node]:
-                    distance[node] = 1 + distance[value]
-                    parent[node] = value
-                    discovered[node] = True  
-                    queue.append(node)
-        if verbose:
-            print(f"Order of Traverseal: {queue}\nDistance:{distance}\nParents: {parent}")
-            return queue, distance, parent     
-        else:
-            return queue, distance, parent
+        if self.type == 'graph':
+            assert type(self.struct) == Graph
 
-    def find_shortest_path(self, goal, start=None):
-        if start is None:
-            start = self.root
+            queue = []  # maintain a queue of visited nodes
+            # Array to keep track whether node is discovered
+            discovered = [False] * len(self.struct.data)
+            distance = [None] * len(self.struct.data)
+            parent = [None] * len(self.struct.data)
+
+            discovered[self.root] = True
+            queue.append(self.root)
+            distance[self.root] = 0
+            idx = 0
+            while idx < len(queue):
+                # dequeue
+                value = queue[idx]
+                idx += 1
+
+                # traversal
+                for node in self.struct.data[value]:
+                    if not discovered[node]:
+                        distance[node] = 1 + distance[value]
+                        parent[node] = value
+                        discovered[node] = True
+                        queue.append(node)
+            if verbose:
+                print(
+                    f"Order of Traverseal: {queue}\nDistance: {distance}\nParents: {parent}")
+                return queue, distance, parent
+            else:
+                return queue, distance, parent
+
+        if self.type == 'tree':
+            assert type(self.struct) == TreeNode
+
+            # Base case
+            if self.struct is None:
+                return
+
+            # creating an empty queue for level order traversal
+            queue = []
+            queue.append(self.struct)
+
+            while len(queue) > 0:
+                print(queue[0].value)
+                node = queue.pop(0)
+
+                if node.left is not None:
+                    queue.append(node.left)
+
+                if node.right is not None:
+                    queue.append(node.right)
+
+    def find_shortest_path(self, target, source=None):
+        """Find the shortest path from source to target"""
+        if source is None:
+            source = self.root
+
+        visited = [False] * len(self.struct.data)
+        parent = [None] * len(self.struct.data)
+        distance = [float('inf')] * len(self.struct.data)
+        queue = []
+
+        distance[source] = 0
+        queue.append(source)
+        idx = 0
+
+        while idx < len(queue) and not visited[target]:
+            # dequeing operation
+            current = queue[idx]
+            visited[current] = True
+            idx += 1
+
+            #update distances
+            self.__update_distances(self.struct, current, distance, parent)
+
+            # find the first unvisited node with the minimum distance
+            next_node = self.__pick_next_node(distance, visited)
+            if next_node:
+                queue.append(next_node)
+
+            visited[current] = True
+
+        return distance[target], parent
+
+    # graph shortest path helper methods
+    def __update_distances(self, graph, current, distance, parent=None):
+        """Update the distances of the current node's neighbors"""
+        neighbors = graph.data[current]
+        weights = graph.weights[current]
+        for i, node in enumerate(neighbors):
+            weight = weights[i]
+            if distance[current] + weight < distance[node]:
+                distance[node] = distance[current] + weight
+                if parent:
+                    parent[node] = current
+
+    def __pick_next_node(distance, visited):
+        """Pick the next univisited node at the smallest distance"""
+        min_distance = float('inf')
+        min_node = None
+        for node in range(len(distance)):
+            if not visited[node] and distance[node] < min_distance:
+                min_node = node
+                min_distance = distance[node]
+        return min_node
 
 
 #* Informed Search Algorithm
